85189220fbad45fc24de782e10d1e5823a761b01
ShawnLi1014 <shawnli1014@gmail.com>
2020-03-06 23:56:58 -0800

    add shot detail frag

--- app/src/main/java/com/sktbd/driboard/ui/fragment/MainFragment.kt
+++ app/src/main/java/com/sktbd/driboard/ui/fragment/MainFragment.kt
@@ -2,6 +2,7 @@ package com.sktbd.driboard.ui.fragment
 
 import android.content.Context
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -25,6 +26,7 @@ class MainFragment : Fragment() {
             inflater, R.layout.fragment_main, container, false)
 
         accessToken = loadData()
+        println(accessToken)
         if (accessToken == "") {
             this.findNavController().navigate(MainFragmentDirections.actionMainFragmentToLoginFragment())
         } else {
