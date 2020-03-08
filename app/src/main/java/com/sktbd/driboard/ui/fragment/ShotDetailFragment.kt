deb546baace03809a1eb70c3b32286788306dbc4
ShawnLi1014 <shawnli1014@gmail.com>
2020-03-07 20:20:39 -0800

    added image to shot detail page

--- app/src/main/java/com/sktbd/driboard/ui/fragment/ShotDetailFragment.kt
+++ app/src/main/java/com/sktbd/driboard/ui/fragment/ShotDetailFragment.kt
@@ -10,11 +10,13 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.engine.DiskCacheStrategy
 import com.google.android.material.chip.Chip
 import com.sktbd.driboard.R
 import com.sktbd.driboard.databinding.FragmentShotDetailBinding
 import com.sktbd.driboard.ui.viewmodel.ShotDetailViewModel
-import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.fragment_shot_detail.*
 import kotlinx.android.synthetic.main.shot_edit_fragment.*
 
 
@@ -22,7 +24,6 @@ class ShotDetailFragment : Fragment() {
 
     private lateinit var viewModel: ShotDetailViewModel
     private lateinit var binding: FragmentShotDetailBinding
-//    val imageView: ImageView = activity!!.findViewById(R.id.shot_detail_image)
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -46,11 +47,15 @@ class ShotDetailFragment : Fragment() {
         viewModel.getShotDetail()
         viewModel.shotInfo.observe(viewLifecycleOwner, Observer { newShotInfo ->
             binding.shotDescription.setText(newShotInfo.description)
-//            Picasso.get().load(newShotInfo.images.normal).into(imageView)
+            binding.collapsingToolbarLayout.title = newShotInfo.title
+            Glide.with(activity!!)
+                .load(newShotInfo.images.normal)
+                .diskCacheStrategy(DiskCacheStrategy.ALL)
+                .into(shot_detail_image);
             newShotInfo.tags.forEach {
                 val chip = Chip(context)
-                chip.text = tag
-                chipGroup?.addView(chip as View)
+                chip.text = it
+                DetailChipGroup?.addView(chip as View)
             }
         })
     }
