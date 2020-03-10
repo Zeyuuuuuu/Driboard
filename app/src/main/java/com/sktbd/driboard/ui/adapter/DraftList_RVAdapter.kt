6a9a957510278327a1400a805f87d37c8e94fff7
Yorkson'MBP <434998827@qq.com>
2020-03-10 12:18:26 -0700

    ✨Update UI

--- app/src/main/java/com/sktbd/driboard/ui/adapter/DraftList_RVAdapter.kt
+++ app/src/main/java/com/sktbd/driboard/ui/adapter/DraftList_RVAdapter.kt
@@ -3,6 +3,7 @@ package com.sktbd.driboard.ui.adapter
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.net.toUri
 import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.sktbd.driboard.R
@@ -11,6 +12,8 @@ import com.sktbd.driboard.databinding.DraftItemBinding
 import com.sktbd.driboard.databinding.ShotItemBinding
 import com.sktbd.driboard.ui.viewmodel.DraftListViewModel
 import kotlinx.android.synthetic.main.draft_item.view.*
+import kotlinx.android.synthetic.main.draft_item.view.textViewTitle
+import kotlinx.android.synthetic.main.shot_item.view.*
 import java.text.SimpleDateFormat
 import java.util.*
 
@@ -51,7 +54,6 @@ class DraftList_RVAdapter(var draftList:List<DraftEntity>, var viewModel: DraftL
         val timeStamp = timeFormatForId.parse(draft.draftID)
         val timeFormartForDisplay = SimpleDateFormat("MM/dd/yyyy HH:mm")
         var time = "Edited on " +timeFormartForDisplay.format(timeStamp)
-
         holder.itemView.textViewDescript.text = time
     }
 
